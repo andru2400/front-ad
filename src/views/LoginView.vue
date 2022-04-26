650a47df1cfe448a161143e0d860c93f53462d97
andru <andru2400@hotmail.com>
2022-04-25 19:18:24 -0500

    firts commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>    
+    <form @submit.prevent="login">
+        <input v-model="form.email" type="email" placeholder="Email ...">
+        <br/>
+        <input v-model="form.password" type="password" placeholder="Password ...">
+        <br/>
+        <button>Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+    data: () => ({
+        form: { 
+            email: '',
+            password: ''
+        }
+    }),
+    methods: { 
+        login() { 
+            console.log("Enviando login ...", this.form)
+        }
+    }
+};
+</script>
+
+<style scoped>
+    input {
+        margin: 2px;
+    }
+</style>
